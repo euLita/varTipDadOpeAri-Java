33b81c5c27f26ccd186eb4e0a3f0bc05704ce51a
euLita <litas.educ@gmail.com>
2022-07-20 23:53:14 -0400

    starting a new repository

---
+++ exercitandoAula02/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,54 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * Classe de exemplo para o exercicio da Aula 2 de Variaveis, Tipos de Dados e Operadores aritmeticos.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+//        short s2 = 40000;
+
+//        int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
+
+//        float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.84d;
+
+        char c1 = 'W';
+//        char c2 = "Tw";
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "ag dgb se et t KNBJBB &*''&%& 75894389";
+
+        String dt1 = "09/02/1981";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+
+
+
+    }
+
+}
